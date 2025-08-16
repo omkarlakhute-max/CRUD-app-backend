2a76fb85f996068d9492f387941021d558a919c1
Omkar Lakhute <omkarlakhutework2@gmail.com>
2025-08-16 05:35:48 +0530

    updates

---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+# ReactJS-CRUD-App
+DevJobs
---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
+import JobPage from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+import EditJobPage from "./pages/EditJobPage";
+
+
+function App() {
+  
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobPage />}/>
+        <Route path="/add-job" element={<AddJobPage />}/>
+        <Route path="/edit-job/:id" element={<EditJobPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
